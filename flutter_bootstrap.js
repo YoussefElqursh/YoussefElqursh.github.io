18f1062ddab73e7799dcfc72a7ce2f4025002f19
youssef883 <79144849+youssef883@users.noreply.github.com>
2024-09-27 14:30:06 +0300

    update website

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3566192343"
+    serviceWorkerVersion: "3068208157"
   }
 });
